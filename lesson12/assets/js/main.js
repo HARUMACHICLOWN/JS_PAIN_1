94180d5a46ed98d996883ceaa6b001e12df22fe8
Asger <111696192+HARUMACHICLOWN@users.noreply.github.com>
2022-11-02 21:17:33 +0300

    Add files via upload

---
+++ lesson12/assets/js/main.js
@@ -0,0 +1,28 @@
+/*let num = 1; 
+while (num <= 100) {
+    console.log(num); 
+    num ++;
+}*/
+
+
+/*let num = 11; 
+while(num <= 33) {
+    console.log(num); 
+    num ++;
+}*/ 
+
+
+/*let num = 0
+while(num < 100) {
+    num++;
+    if(num % 2 == 0) console.log(num)
+}*/
+
+
+
+
+
+
+
+
+
